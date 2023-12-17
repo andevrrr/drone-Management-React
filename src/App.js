4497e0b2e4b12759d8950c4b77690347c4fb3df1
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-17 15:52:57 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import DronePage from './Pages/Drones/drones';
 import OrderPage from './Pages/Orders/orders';
 
 function App() {
   return (
-    <div className="App">
-      <DronePage />
-      <OrderPage />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/drones">Drones</Link>
+            </li>
+            <li>
+              <Link to="/orders">Orders</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/drones" element={<DronePage />} />
+          <Route path="/orders" element={<OrderPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
